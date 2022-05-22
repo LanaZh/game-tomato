a8a6af84c528dd67ba5685b09104e9f3f2d13a32
ZhukovaSveta <thelumierlana@yandex.ru>
2022-05-22 20:43:08 +0300

    bezier timer

---
+++ game-tomato/game-tomato/BezierTimer.swift
@@ -0,0 +1,29 @@
+//
+//  BezierTimer.swift
+//  game-tomato
+//
+//  Created by Надежда Жукова on 22.05.2022.
+//
+
+import UIKit
+
+class BezierTimer: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
